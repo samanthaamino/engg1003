d38be34fba597e7019629e571c95c16994b0a822
Samantha Amino <c3355702@uon.edu.au>
2020-02-24 20:05:28 +1100

    initial commit

---
+++ main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    float x;
+    float y;
+    printf("Enter an number: ");
+    scanf("%f", &x);
+    y = 2 * x + 3 * 5;
+    printf("y: %f\n", y);
+    return 0;
+}
